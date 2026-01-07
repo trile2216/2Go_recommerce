3a7dfe0a9d1433b75b0f9edac04b62b4e9415118
trile2216 <trile9270@gmail.com>
2026-01-07 21:20:42 +0700

    Update Program.cs

--- BE/2GO_EXE_Project.API/Program.cs
+++ BE/2GO_EXE_Project.API/Program.cs
@@ -80,16 +80,16 @@ if (FirebaseApp.DefaultInstance == null)
 {
     var credentialPath = firebaseSection["CredentialsPath"] ?? builder.Configuration.GetValue<string>("Firebase__CredentialsPath");
     var projectId = firebaseSection["ProjectId"] ?? builder.Configuration.GetValue<string>("Firebase__ProjectId");
+    var credentialJson = firebaseSection["CredentialsJson"] ?? builder.Configuration.GetValue<string>("firebase-credentials");
 #pragma warning disable CS0618 // FromFile is marked obsolete in this version; acceptable for setup
     GoogleCredential credential;
-    var envPath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
     if (!string.IsNullOrWhiteSpace(credentialPath) && File.Exists(credentialPath))
     {
         credential = GoogleCredential.FromFile(credentialPath);
     }
-    else if (!string.IsNullOrWhiteSpace(envPath) && File.Exists(envPath))
+    else if (!string.IsNullOrWhiteSpace(credentialJson) && File.Exists(credentialJson))
     {
-        credential = GoogleCredential.FromFile(envPath);
+        credential = GoogleCredential.FromJson(credentialJson);
     }
     else
     {
